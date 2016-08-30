9f630481da11f95a659b7302c520fe04233d306a
Philipp Bogensberger <philipp@crate.io>
2016-08-31 00:42:32 +0200

    formatting

--- var/www/index.php
+++ var/www/index.php
@@ -3,14 +3,12 @@
   <title>PHP Test</title>
  </head>
  <body>
-<?php
- echo '<p>Hello Word</p>';
- $dbh = @pg_connect("host=localhost dbname=doc");
- $result = @pg_query($dbh, "select version['number'] from sys.nodes limit 1");
-
- $row = @pg_fetch_array($result, 0, PGSQL_NUM);
-
+ <?php
+  echo '<p>Hello Word</p>';
+  $dbh = @pg_connect("host=localhost dbname=doc");
+  $result = @pg_query($dbh, "select version['number'] from sys.nodes limit 1");
+  $row = @pg_fetch_array($result, 0, PGSQL_NUM);
   echo '<p>Crate is running with version '.$row[0].'</p>';
-?>
+ ?>
  </body>
 </html>
